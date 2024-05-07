f6e8cdebbf1fa3403846c94b95b8d9b471ca6d00
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-05-07 13:17:34 +0530

    Captcha Generator ✓

---
+++ ProjectDay5/script.js
@@ -0,0 +1,43 @@
+const display = document.getElementById('status');
+const body = document.body;
+const submit = document.getElementById('submit');
+const refresh = document.getElementById('refresh');
+
+const char = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+let captcha = "";
+
+body.onload = function generateCaptcha() {
+    const captchaLength = 6;
+    for(let i = 0; i<captchaLength; i++) {
+        const randomIndex = Math.floor(Math.random() * char.length);
+        captcha += char.substring(randomIndex, randomIndex + 1);
+    }
+
+    document.getElementById('generator').value = captcha;
+    display.innerText = 'Captcha Generator'
+}
+
+submit.onclick = function checkInput() {
+    const input = document.getElementById('client-text').value;
+
+    if(input === "") {
+        display.innerText = "Please Enter the text Shown Below 👇";
+    } else if(input === captcha) {
+        display.innerText = 'Matched😎';
+    }else{
+        display.innerText = "Not-Matched😖";
+    }
+
+}
+
+refresh.onclick = function refreshCaptcha() {
+    let newCaptcha = "";
+    for(let i = 0; i < captcha.length; i++) {
+        const randomChar = char[Math.floor(Math.random() * char.length)]
+        newCaptcha += randomChar;
+    }
+    captcha = newCaptcha;
+    document.getElementById('generator').value = captcha;
+    display.innerText = 'Captcha Generator';
+}
