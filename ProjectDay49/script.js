3a673301f525fb0bdef3332fc23c4ca15bcf2701
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2024-06-24 08:05:36 +0530

    Typing Text Effect ✓

---
+++ ProjectDay49/script.js
@@ -0,0 +1,26 @@
+const dynamicText = document.querySelector('h1 span');
+const words = ["Fun", "Love", "the World", "Life"];
+
+let wordIndex = 0;
+let charIndex = 0;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChars = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChars;
+
+    if(charIndex < currentWord.length) {
+        charIndex++;
+        console.log(charIndex);
+        setTimeout(typeEffect,100);
+    } else {
+        charIndex = 0;
+        if(wordIndex >= words.length - 1) {
+            wordIndex = 0;
+        } else {
+            wordIndex++;
+        }
+        setTimeout(typeEffect, 1000);
+    }
+}
+typeEffect();
